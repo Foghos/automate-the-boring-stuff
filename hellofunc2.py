de5c7fce02f021064df5962f9c15dd93b49ddfbb
Foghos <76908423+Foghos@users.noreply.github.com>
2021-01-05 17:55:20 +0000

    Add files via upload

---
+++ hellofunc2.py
@@ -0,0 +1,6 @@
+# i gave the function hello the parameter name--> it's a variable
+def hello(name):
+    print("Hello, " + name)
+# Alice and Bob are called Arguments
+hello("Alice")
+hello("Bob")
---
+++ keyword arguments & the print() function.py
@@ -0,0 +1,7 @@
+# end="" prints on the same line
+print("Hello", end="")
+print("World")
+
+# sep=","--> the variables are printed with a separator in between them
+print("cats", "dogs", "mice", sep=",")
+
