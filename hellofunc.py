470f49c21f697a20e0b7f3b3951f8acf18f9ab82
Foghos <76908423+Foghos@users.noreply.github.com>
2021-01-07 17:28:00 +0000

    Delete hellofunc.py

--- hellofunc.py
+++
@@ -1,9 +0,0 @@
-# First you have to define a function.
-def hello():
-    print("Howdy!")
-    print("Howdy!!!")
-    print("Hello there.")
-# To execute the code inside of the function you have to call the function.
-hello()
-hello()
-hello()
